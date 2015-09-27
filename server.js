b80d837c0032bd24f4c940e8a4f5544aa62dac9e
Joakim Forsberg <joakim.forsberg@readly.com>
2015-09-27 17:35:49 +0200

    Add story page with chracters, indeed not good to make two calls to the api for story page but thats how it is

--- server.js
+++ server.js
@@ -26,17 +26,19 @@ hbs.registerHelper( "resurceToId", function (resourceURI) {
 });
 
 hbs.registerHelper( "marvelThumb", function ( thumbnail, size ) {
-  size = "portrait_xlarge"
+
   switch(size) {
     case "large":
       size = "portrait_xlarge"
-    break;
+      break;
     case "medium":
       size = "portrait_medium"
-    break;
+      break;
     case "small":
       size = "portrait_small"
-    break;
+      break;
+    default:
+      size = "portrait_xlarge"
   }
   return build_thumb_src = thumbnail.path + "/" + size + ".jpg"
 });
@@ -84,10 +86,36 @@ app.get('/character/:id(\\d+)/', function(req, res) {
   }
 
   request(options, callback)
-
-
 })
+app.get('/story/:id(\\d+)', function(req, res) {
+  var story_id   = req.params.id
+  var story_uri  = config.marvel.end_point + "/v1/public/stories/"+story_id+'?'+scripts.Marvel.auth_query()
+  var options    = { url: story_uri, method: "GET" }
+  // Due to not getting thumbnails from characters when we get stories we need to make another api call, yea not good.
+  var story_characters_uri  = config.marvel.end_point + "/v1/public/stories/"+story_id+'/characters?'+scripts.Marvel.auth_query()
+  var options_c    = { url: story_characters_uri, method: "GET" }
 
+  function callback(error, response, body) {
+    var story = JSON.parse(body);
+    //console.log(story)
+
+    if(story.data) {
+
+      function callback(error_c, response_c, body_c) {
+        story_characters = JSON.parse(body_c)
+        console.log(story_characters.data.results)
+        res.render('story', {story: story.data.results[0]
+                             ,story_characters: story_characters.data.results})
+      }
+      request(options_c, callback)
+    } else {
+      alert("dsadasda")
+    }
+
+  }
+
+  request(options, callback)
+})
 var port = process.env.PORT || 3000
 
 app.listen(port, function() {
